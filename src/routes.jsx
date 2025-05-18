f60141484d41867d2987acecbe63ba51c9e79f9c
Mohammad-Mehdi-Delavari <mahdidlavary@gmail.com>
2025-05-18 11:57:44 +0330

    add builded file

--- src/routes.jsx
+++
@@ -1,6 +0,0 @@
-import PageNote from "./pages/PageNot";
-import Home from "./pages/Home";
-export let routes = [
-  { path: "/", element: <Home /> , title:"Home" },
-  { path: "/*", element: <PageNote /> ,title:'404'},
-];
